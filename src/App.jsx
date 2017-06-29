a81f55d6223f64de1f3e3aba8a5adb4848030950
Haosik <noobbaster@gmail.com>
2017-06-30 01:06:02 +0300

    Validation message. More adequate submit

--- src/App.jsx
+++ src/App.jsx
@@ -8,32 +8,42 @@ export class App extends Component {
 		super(props)
 
 		this.state = {
-			newDeadline: 'December 25, 2017',
-			deadline: 'December 25, 2017'
+			newDeadline: '',
+			deadline: 'December 25, 2017',
+			validDeadline: true
 		}
 	}
+
 	newDeadlineHandler = (e) => {
-		if (Date.parse(e.target.value)) {
+		this.setState({
+			newDeadline: e.target.value
+		});
+	};
+
+	setDeadlineHandler = (e) => {
+		e.preventDefault();
+		if (Date.parse(this.state.newDeadline)) {
 			this.setState({
-				newDeadline: e.target.value
+				validDeadline: true,
+				deadline: this.state.newDeadline
+			})
+		} else {
+			this.setState({
+				validDeadline: false
 			});
 		}
-	}
-	setDeadlineHandler = () => {
-		this.setState({
-			deadline: this.state.newDeadline
-		})
-	}
+	};
 	render() {
 		return (
-			<div className="counter__wrap">
+			<div className="counter__wrap container">
 				<h4 className="counter__heading">Countdown to {this.state.deadline}</h4>
 				<Clock deadline={this.state.deadline} />
-				<Form inline className="form counter__form" action="#">
+				<Form inline onSubmit={(e) => this.setDeadlineHandler(e)} className="form counter__form" action="">
 					<InputGroup>
 						<FormControl onChange={(e) => this.newDeadlineHandler(e)} className="form-control" name="deadlineInput" type="text" placeholder="Enter new date" />
-						<InputGroup.Button><Button onClick={() => this.setDeadlineHandler()}>Submit</Button></InputGroup.Button>
+						<InputGroup.Button><Button onClick={(e) => this.setDeadlineHandler(e)}>Submit</Button></InputGroup.Button>
 					</InputGroup>
+					{this.state.validDeadline ? <div></div> : <div className="counter__error">Please, enter some valid Date! (e.g. November 25 2017)</div>}
 				</Form>
 			</div>
 		)
